897ac0537cb1542213aa5f474c07a6c9c25fe2d0
d.stukov <d.stukov@skbkontur.ru>
2022-04-28 10:12:12 +0500

    Use coroutines to save data in db

--- app/src/main/java/com/example/task2/viewmodels/EditHabitViewModel.kt
+++ app/src/main/java/com/example/task2/viewmodels/EditHabitViewModel.kt
@@ -4,11 +4,19 @@ import android.graphics.Color
 import androidx.lifecycle.ViewModel
 import com.example.task2.models.HabitData
 import com.example.task2.storage.HabitRepository
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class EditHabitViewModel : ViewModel() {
+class EditHabitViewModel : ViewModel(), CoroutineScope {
+    private val job = SupervisorJob()
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job + CoroutineExceptionHandler{ _, e -> throw  e}
+
     var borderColor: Int = Color.parseColor("#00EF10")
 
-    fun addOrUpdate(habit: HabitData) {
-        HabitRepository().addOrUpdate(habit)
+    fun addOrUpdate(habit: HabitData) = launch {
+        withContext(Dispatchers.IO) {
+            HabitRepository().addOrUpdate(habit)
+        }
     }
 }
