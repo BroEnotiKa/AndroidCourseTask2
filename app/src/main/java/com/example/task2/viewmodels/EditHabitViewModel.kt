95449f829fdc43709c10aba126e7c80fb68ac337
d.stukov <d.stukov@skbkontur.ru>
2022-05-10 12:59:50 +0500

    use viewModelScope instead of CoroutineScope

--- app/src/main/java/com/example/task2/viewmodels/EditHabitViewModel.kt
+++ app/src/main/java/com/example/task2/viewmodels/EditHabitViewModel.kt
@@ -2,19 +2,15 @@ package com.example.task2.viewmodels
 
 import android.graphics.Color
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.task2.models.HabitData
 import com.example.task2.storage.HabitRepository
 import kotlinx.coroutines.*
-import kotlin.coroutines.CoroutineContext
 
-class EditHabitViewModel : ViewModel(), CoroutineScope {
-    private val job = SupervisorJob()
-    override val coroutineContext: CoroutineContext
-        get() = Dispatchers.Main + job + CoroutineExceptionHandler{ _, e -> throw  e}
-
+class EditHabitViewModel : ViewModel() {
     var borderColor: Int = Color.parseColor("#00EF10")
 
-    fun addOrUpdate(habit: HabitData) = launch {
+    fun addOrUpdate(habit: HabitData) = viewModelScope.launch {
         withContext(Dispatchers.IO) {
             HabitRepository().addOrUpdate(habit)
         }
